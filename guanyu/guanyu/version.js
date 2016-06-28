1c6d352385e8755cd44dab7dfdef5e3407f01a5d
Cliff Chao-kuan Lu <clifflu@gmail.com>
2016-06-28 15:00:09 +0800

    feat: supply version from package.json

--- guanyu/guanyu/version.js
+++ guanyu/guanyu/version.js
@@ -1,1 +1,6 @@
-module.exports = '0.0.3';
+var fs = require('fs');
+var path = require('path');
+
+var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'));
+
+module.exports = pkg.version || 'unknown';
