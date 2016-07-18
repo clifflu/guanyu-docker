e7f491362656e78368aff35a417b657637633e57
Cliff Chao-kuan Lu <clifflu@gmail.com>
2016-07-18 14:24:22 +0800

    fix: FILE:MAX_SIZE instead of old, env-based customization

--- guanyu/guanyu/scanner/uri.js
+++ guanyu/guanyu/scanner/uri.js
@@ -12,7 +12,7 @@ var logger = require('../logger');
 var mycache = require('../cache');
 var myhash = require("../hash");
 
-var file_max_size = require('../config').file_max_size;
+var file_max_size = require('../config').get('FILE:MAX_SIZE');
 
 
 var host_whitelist = [
