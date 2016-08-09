db281b559180621c2529c02dcbcfd98f1ddf0ad7
Cliff Chao-kuan Lu <clifflu@gmail.com>
2016-08-09 14:37:52 +0800

    refactor: centralize semaphores

---
+++ guanyu/guanyu/sem.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const sem = require('semaphore');
+
+const config = require('./config');
+
+const cpu_count = os.cpus().length;
+
+const sav_seats = config.get('SCAN:PER_CORE') * cpu_count;
+const fetch_seats = config.get('FETCH:PER_CORE') * cpu_count;
+const conn_seats = config.get('CONN:RATIO') * (sav_seats + fetch_seats) * cpu_count;
+
+module.exports = {
+  fetch: sem(fetch_seats),
+  sav: sem(sav_seats),
+  seats: {
+    conn: conn_seats,
+    fetch: fetch_seats,
+    sav: sav_seats,
+  }
+};
