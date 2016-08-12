fe29553e1b2479c1bfba590ab8c9e9f309e7af70
Cliff Chao-kuan Lu <clifflu@gmail.com>
2016-08-12 14:27:47 +0800

    fix: logic error on `conn_seats`, secure all seats as well

--- guanyu/guanyu/sem.js
+++ guanyu/guanyu/sem.js
@@ -7,9 +7,9 @@ const config = require('./config');
 
 const cpu_count = os.cpus().length;
 
-const sav_seats = config.get('SCAN:PER_CORE') * cpu_count;
-const fetch_seats = config.get('FETCH:PER_CORE') * cpu_count;
-const conn_seats = config.get('CONN:RATIO') * (sav_seats + fetch_seats) * cpu_count;
+const sav_seats = Math.floor(config.get('SCAN:PARALLEL') || config.get('SCAN:PER_CORE') * cpu_count);
+const fetch_seats = Math.floor(config.get('FETCH:PARALLEL') || config.get('FETCH:PER_CORE') * cpu_count);
+const conn_seats = Math.floor(config.get('CONN:RATIO') * (sav_seats + fetch_seats));
 
 module.exports = {
   fetch: sem(fetch_seats),
