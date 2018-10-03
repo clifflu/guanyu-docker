c85ac7c6684fd25462a5b763a1060c1e870aa171
hank9653 <t102318120@ntut.org.tw>
2018-10-03 12:18:44 +0800

    feat: fetch sophosav queue message
    add fetch.js to listen sophosav queue

--- sophosav/index.js
+++ sophosav/index.js
@@ -1,9 +1,32 @@
 const gc = require('guanyu-core')
+const main = require('./src/fetch')
+
+const config = gc.config
 const plogger = gc.prepareLogger
+
+function resurrectFromDoom(err) {
+  const logger = plogger({
+    loc: 'sophosav:index:resurrectFromDoom',
+    extra: {stack: err.stack}
+  })
+
+  logger.error(`${err.name}: ${err.message}`)
+
+  return new Promise(resolve => setTimeout(resolve, config.get('DOOM_SLEEP')))
+}
+
+function loop(){
+  main()
+    .catch(resurrectFromDoom)
+    .then(() => setTimeout(loop, 1000 * config.get('PLUGIN:FETCH:EMPTY_SLEEP')))
+}
 
 function entry() {
   const logger = plogger({loc: 'sophosav:index:entry'})
-  logger.info(`entry sophosav`)
+  logger.info(`listening from ${config.get('PLUGIN:SOPHOSAV:QUEUE')}`)
+
+  loop()
 }
 
 entry()
+
---
+++ sophosav/src/fetch.js
@@ -0,0 +1,91 @@
+const gc = require('guanyu-core')
+
+const config = gc.config
+const logFn = 'sophosav:src/fetch'
+const plogger = gc.prepareLogger
+const sqs = new gc.aws.SQS()
+
+const sqsParams = Object.freeze({
+  QueueUrl: config.get('PLUGIN:SOPHOSAV:QUEUE'),
+  WaitTimeSeconds: config.get('PLUGIN:FETCH:WAIT_SECONDS'),
+})
+
+function receiveMessage() {
+  const logger = plogger({loc: `${logFn}:receiveMessage`})
+
+  return new Promise((resolve, reject) => {
+    sqs.receiveMessage(sqsParams, (err, data) => err ? reject(err) : resolve(data))
+  })
+}
+
+function parseMessageSync(awsData) {
+  const logger = plogger({loc: `${logFn}:parsePayload`})
+
+  if (!awsData.Messages) {  // No data
+    logger.info(`empty queue`)
+    return {}
+  }
+
+  let body
+  let msg = awsData.Messages[0]
+  let awsMsgId = msg.MessageId
+  let handle = msg.ReceiptHandle
+
+  try {
+    body = JSON.parse(msg.Body)
+  } catch (e) {
+    // malformed body, log and mark for deletion
+    logger.warn(`malformed body: ${JSON.stringify(msg.Body)}`)
+    return { handle }
+  }
+
+  logger.info(`received msgId: ${awsMsgId}`)
+
+  return { handle, awsMsgId, body }
+}
+
+function processMessage(payload) {
+  const logger = plogger({loc: `${logFn}:processMessage`})
+
+  if (!payload.body) {  // No resource
+    logger.info(`queue message is empty`)
+    return {}
+  }
+
+  return payload;
+}
+
+function deleteMessage(payload) {
+  const logger = plogger({loc: `${logFn}:deleteMessage`})
+
+  if (!payload.handle) {
+    logger.info(`skipping ${payload.awsMsgId} for deleteion for handle missing`)
+    return payload
+  }
+
+  let params = {
+    QueueUrl: config.get('PLUGIN:SOPHOSAV:QUEUE'),
+    ReceiptHandle: payload.handle,
+  }
+
+  return new Promise((resolve, reject) => {
+    sqs.deleteMessage(params, (err, data) => {
+      if (err)
+        return reject(err)
+
+      logger.info(`deleted msgId ${payload.awsMsgId}`)
+      resolve(payload)
+    })
+  })
+
+  return payload
+}
+
+function main() {
+  return receiveMessage()
+    .then(parseMessageSync)
+    .then(processMessage)
+    .then(deleteMessage)
+}
+
+module.exports = main
