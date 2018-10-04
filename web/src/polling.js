a356ed33adee110b1954fb903ca6b49ed9dce518
tod.shen <tod.shen@104.com.tw>
2018-10-05 00:15:35 +0800

    fix: resolve return error.

--- web/src/polling.js
+++ web/src/polling.js
@@ -7,13 +7,15 @@ let timerID;
 
 function end() {
   const logger = prepareLogger({ loc: `${logFn}:end` });
+  logger.debug("polling stop");
   pullID = pullID && clearInterval(pullID);
   timerID = timerID && clearTimeout(timerID);
 }
 
-function checkResult(payload) {
+function checkResult(resolve, reject) {
   const logger = prepareLogger({ loc: `${logFn}:checkResult` });
-  return new Promise((resolve, reject) => {
+  return (payload) => {
+    logger.debug("check payload", payload);
     if (payload.status || payload.result) {
       end();
       cache.update_result_naive(payload).then(() => {
@@ -23,7 +25,7 @@ function checkResult(payload) {
         resolve(payload);
       });
     }
-  });
+  }
 }
 
 function get_result(payload) {
@@ -34,7 +36,7 @@ function get_result(payload) {
       reject(payload);
     }, 30 * 1000);
     pullID = setInterval(() => {
-      cache.get_result_ddb(payload).then(checkResult);
+      cache.get_result_ddb(payload).then(checkResult(resolve, reject));
     }, 1000);
   });
 }
