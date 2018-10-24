d63b6ee331ec1afdeb6bb0b6d75707b21da4399e
tod.shen <tod.shen@104.com.tw>
2018-10-24 23:09:26 +0800

    refactor: web polling change interval time

--- web/src/polling.js
+++ web/src/polling.js
@@ -3,6 +3,7 @@ const logFn = "web:src/polling";
 
 function polling(payload) {
   const logger = prepareLogger({ loc: `${logFn}:polling` });
+  let time = [0, 1];
   let pullID;
   let timerID;
 
@@ -15,31 +16,56 @@ function polling(payload) {
     timerID = timerID && clearTimeout(timerID);
   };
 
-  function checkResult(resolve, reject) {
+  function checkResult() {
     return (payload) => {
       if (payload.status || payload.result) {
         end();
         logger.debug("Polling stop");
         cache.update_result_naive(payload).then(() => {
           if (payload.status) {
-            return reject(payload);
+            return this.reject(payload);
           }
-          resolve(payload);
+          return this.resolve(payload);
         });
+      }
+      if (timerID) {
+        clearInterval(pullID);
+        startInterval();
       }
     }
   };
 
+  function setEndMethod(resolve, reject) {
+    this.resolve = resolve;
+    this.reject = reject;
+  }
+
+  function startInterval() {
+    pullID = setInterval(() => {
+      cache.get_result_ddb(payload).then(checkResult());
+    }, getIntervalTime() * 1000);
+  }
+
+  function getIntervalTime() {
+    let intervalTime = time[0] + time[1];
+
+    if (intervalTime < 5) {
+      time[0] = time[1];
+      time[1] = intervalTime;
+    }
+
+    return intervalTime;
+  }
+
   logger.debug("Polling start...");
   return new Promise((resolve, reject) => {
+    setEndMethod(resolve, reject);
     timerID = setTimeout(() => {
       end();
       logger.debug("Polling timeout");
       reject(payload);
     }, (payload.responseTime || 60) * 1000);
-    pullID = setInterval(() => {
-      cache.get_result_ddb(payload).then(checkResult(resolve, reject));
-    }, 1000);
+    startInterval();
   });
 }
 
